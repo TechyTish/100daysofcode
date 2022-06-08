1284b0b22488b676c0047bb14090a0f99f2d691e
TechyTish <xotish@live.co.uk>
2022-06-08 20:57:52 +0100

    update file names

---
+++ day1/ex3-comments.py
@@ -0,0 +1,12 @@
+# this is an example of a comment on 1 line
+print ("you can watch my videos here https://youtube.com/c/techytish")
+
+
+'''
+this is a block comment
+please ignore
+this line
+and this one
+''' 
+
+print ("hello")
