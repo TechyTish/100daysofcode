a546bfc27b2e1b293b1a37fc78b9e98692b2b3da
TechyTish <xotish@live.co.uk>
2022-06-08 22:03:03 +0100

    complete ex6

---
+++ day1/ex5-variables.py
@@ -0,0 +1,12 @@
+name = input("what is your name? ")
+print(name)
+
+name = "techy"
+print(name)
+
+name = "tish"
+print(name)
+
+age = input("what is your age? ")
+length = len(age)
+print(length)
---
+++ day1/ex6-variables_part2.py
@@ -0,0 +1,16 @@
+a = input("a: ")
+b = input("b: ")
+
+####################################
+#Write your code below this line 👇
+
+temp = a
+a = b
+b = temp
+
+#Write your code above this line 👆
+####################################
+
+# 🚨 Don't change the code below 👇
+print("a: " + a)
+print("b: " + b)
