4864b8e8b9573e19039ab2f40e9076ad71a119ee
TechyTish <xotish@live.co.uk>
2022-06-09 17:00:58 +0100

    attempt 1 - fail

---
+++ day2/ex3-data_types_part3.py
@@ -0,0 +1,15 @@
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+
+print(type(two_digit_number))
+conversion = int(two_digit_number)
+print(type(conversion))
+print(conversion[0])
+
+
+
+
