5707460921373d9d021ed10ba6d105df59f37c6f
TechyTish <xotish@live.co.uk>
2022-06-09 20:23:00 +0100

    update calculations

--- day2/ex6_life_in_weeks.py
+++ day2/ex6_life_in_weeks.py
@@ -9,18 +9,13 @@ days = 365
 weeks = 52
 months = 12
 
+90 - 28 = 62
+
 #how many total days to live 90 years
-age_days = int(days * final_age / int(age))
-
+age_days = int((final_age - int(age)) * days)
 #how many total weeks to live 90 years
-age_weeks = int(weeks * final_age / int(age))
-
+age_weeks = int((final_age - int(age)) * weeks)
 #how many total months to live 90 years
-age_months = int(months * final_age / int(age))
+age_months = int((final_age - int(age)) * months)
 
 print(f"you have {age_days} days, {age_weeks} weeks, and {age_months} months left until you are {final_age} years old.")
-
-
-
-
-
