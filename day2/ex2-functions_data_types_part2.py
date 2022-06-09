aab17ea5fcc8cbe73b8d27596a2cfcaa29398693
TechyTish <xotish@live.co.uk>
2022-06-09 16:36:00 +0100

    add ex 2

---
+++ day2/ex2-functions_data_types_part2.py
@@ -0,0 +1,9 @@
+num_char = len(input("What is your name? "))
+
+#check the type of a var
+print(type(num_char))
+
+#type conversion aka type casting
+#convert integer to string
+new_num_char = str(num_char)
+print("Your name has " + new_num_char + " characters.")
