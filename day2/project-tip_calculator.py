053b21942a1b19e4107da794b6a7c515979da5cb
TechyTish <xotish@live.co.uk>
2022-06-09 22:24:00 +0100

    attempt 1 -fail type error

--- day2/project-tip_calculator.py
+++ day2/project-tip_calculator.py
@@ -6,3 +6,20 @@
 #Tip: There are 2 ways to round a number. You might have to do some Googling to solve this.💪
 
 #Write your code below this line 👇
+
+print("Welcome to the tip calculator.")
+starting_bill = input(float("What was the total bill? £"))
+
+#percentages of tip options
+percent_10 = 10
+percent_12 = 12
+percent_15 = 15
+
+percentage = input(int(f"What percentage tip would you like to give? {percent_10}, {percent_12} or {percent_15}? "))
+percentage = int((percentage / 100) * starting_bill)
+
+split_no = input(int("How many people to split the bill? "))
+
+final_bill = round(percentage / split_no) ** 2
+
+print(f"Each person should pay: £{final_bill}")
