5edaa1e797de1abcfb552e250003a744a048e0dc
TechyTish <xotish@live.co.uk>
2022-06-09 22:56:44 +0100

    add the rounding of the final bill to 2 decimal places

--- day2/project-tip_calculator.py
+++ day2/project-tip_calculator.py
@@ -22,6 +22,6 @@ bill_and_tips = starting_bill_float + tips
 split_no = input("How many people to split the bill? ")
 split_no_int = int(split_no)
 
-final_bill = bill_and_tips / split_no_int
+final_bill = (round(bill_and_tips / split_no_int, 2))
 
 print(f"Each person should pay: £{final_bill}")
