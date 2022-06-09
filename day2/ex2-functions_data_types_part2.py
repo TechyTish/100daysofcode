a4a23f0fa8f597cb501718f6a4482c027de9fd3a
TechyTish <xotish@live.co.uk>
2022-06-09 16:40:08 +0100

    add more function conversions

--- day2/ex2-functions_data_types_part2.py
+++ day2/ex2-functions_data_types_part2.py
@@ -7,3 +7,15 @@ print(type(num_char))
 #convert integer to string
 new_num_char = str(num_char)
 print("Your name has " + new_num_char + " characters.")
+
+a = 123
+print(type(a))
+
+a = str(123)
+print(type(a))
+
+#adds an integer to float
+print(70 + float("100.5"))
+
+#adds strings together
+print(str(70)+ str(100))
