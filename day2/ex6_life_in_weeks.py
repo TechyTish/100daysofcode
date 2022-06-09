a4981466ba011964b1b96ee6283123be1856d1f6
TechyTish <xotish@live.co.uk>
2022-06-09 19:47:41 +0100

    adding the vars and final print statement

--- day2/ex6_life_in_weeks.py
+++ day2/ex6_life_in_weeks.py
@@ -4,9 +4,14 @@ age = input("What is your current age?")
 
 #Write your code below this line 👇
 
+final_age = 90
+days = 31
+weeks = 52
+months = 12
 
 
 
+print(f"you have {days} days, {weeks} weeks, and {408} months left until you are {final_age} years old.")
 
 
 
