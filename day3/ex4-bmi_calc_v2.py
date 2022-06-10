71cdd666f83735b4c385f75438616ba24b09fc07
TechyTish <xotish@live.co.uk>
2022-06-10 23:34:33 +0100

    check bmi

---
+++ day3/ex4-bmi_calc_v2.py
@@ -0,0 +1,36 @@
+# 🚨 Don't change the code below 👇
+from multiprocessing.context import assert_spawning
+
+
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+height_result = float(height) ** 2
+weight_result = int(weight)
+
+bmi_result = round(weight_result / height_result) ** 0
+
+bmi_category = ""
+
+print(f"your bmi is: {bmi_result}")
+
+if bmi_result < 18.5:
+  bmi_category = "underweight"
+
+if bmi_result > 18.5 < 25:
+  bmi_category = "normal weight"
+
+if bmi_result > 25 < 30:
+  bmi_category = "slightly overweight"
+
+if bmi_result > 30 < 35:
+  bmi_category = "obese"
+
+if bmi_result > 35:
+  bmi_category = "clinically obese"
+
+#convert bmi result to integer
+print(f"Your BMI is {bmi_result}, and you are {bmi_category}!")
