9a9c6cc02c8ad6bcfd14087d1de1ff7afafc0b85
TechyTish <xotish@live.co.uk>
2022-06-11 18:41:09 +0100

    ex5 solution

--- day3/ex5-leap_year.py
+++ day3/ex5-leap_year.py
@@ -4,11 +4,14 @@ number = int(input("What year do you want to check? "))
 #empty string
 leap_year_result = ""
 
+#nested if statmeent
 if (number % 4) == 0:
+  leap_year_result = "leap year"
   if (number % 100) == 1:
+    leap_year_result = "leap year"
     if (number % 400) == 0:
-      leap_year_result = "this is a leap year"
+      leap_year_result = "leap year"
 else:
-    leap_year_result = "this is a not a leap year"
+  leap_year_result = "not a leap year"
 
 print(f"Result: {leap_year_result}")
